818ef418553141d4647461fb77e65ecccd31efde
ozeron <ozeron@me.com>
2018-01-14 00:32:17 +0200

    ADDED MODEL FROM CHALENGE

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,9 @@
-def greet():
-    print("Hello world!")
-    
+import pandas as pd
+
+from os import path 
+
+
+PROJECT_HOME = path.normpath(path.join(__file__, '../..'))
+def load_dataframe(mode):
+    file_path = path.join(PROJECT_HOME, 'data', mode, 'data.json')
+    return pd.read_json(file_path)
